c6efec35ba3fcdcb44e946915efa633a9bdcebfa
dmsqja <bum17822@naver.com>
2026-08-31 17:10:01 +0900

    chore: Prisma 7 업그레이드 설정 확인

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,14 +1,16 @@
-import { PrismaClient, Prisma } from "@prisma/client";
-import { isProduction } from "./env";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+import { env, isProduction } from "./env";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 
 export const prisma = new PrismaClient({
+  adapter,
   log: isProduction ? ["error"] : ["query", "warn", "error"],
 });
 
 /** 트랜잭션 컨텍스트. service에서 tx를 주고받을 때 사용합니다. */
 export type PrismaTransaction = Omit<
-  PrismaClient,
-  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
+  typeof prisma,
+  "$connect" | "$disconnect" | "$on" | "$transaction" | "$extends"
 >;
-
-export { Prisma };
