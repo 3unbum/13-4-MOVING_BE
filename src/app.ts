f397b96c66804fa229728b8b4ebd4a3d69db03bc
HoneyLatlll <rlaalstn9415@naver.com>
2026-09-01 13:48:03 +0900

    refactor: 쿠키 기반 인증 코드 제거
    BFF 원칙(쿠키 미사용, JSON body로만 토큰 응답)에 맞춰
    cookie-parser 및 CORS credentials 옵션 제거

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import { env, isTest } from "./config/env";
 import routes from "./routes";
@@ -8,10 +7,9 @@ import { errorHandler, notFoundHandler } from "./common/middlewares/errorHandler
 
 const app = express();
 
-app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: env.CLIENT_URL }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 if (!isTest) app.use(morgan("dev"));
 
 app.use("/api", routes);
