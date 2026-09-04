07fe39f5ed6604652f56106392d9f29a1f579be2
HoneyLatlll <rlaalstn9415@naver.com>
2026-09-04 14:12:41 +0900

    chore: OAuthProviderProfile name 필드 주석 정정

--- src/modules/auth/oauth/types.ts
+++ src/modules/auth/oauth/types.ts
@@ -2,7 +2,7 @@
 export type OAuthProviderProfile = {
   providerId: string;
   email: string;
-  /** 카카오/네이버는 동의 항목에 따라 빈 문자열일 수 있음 */
+  /** 콘솔에서 필수 동의 항목으로 걸어둬도 외부 API 응답이라 방어적으로 빈 문자열 fallback을 둠 */
   name: string;
   profileImage: string | null;
 };
